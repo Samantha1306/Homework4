8e920f494985eee48b6b7002cf63aea0c4232dff
Samantha <samantha@mail.com>
2022-11-02 20:22:34 +0300

    add programme Ex29

---
+++ Ex29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+try
+{
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"Введите целое число {index + 1} для заполнения массива из 8 элементов: ");
+        collection[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = 8;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
+}
+catch
+{
+    Console.WriteLine ($"Ошибка! Введите целое число.");
+}
