50769cea2b436972e0022a0a8ef13d40580808c1
Samantha <samantha@mail.com>
2022-11-02 20:43:23 +0300

    add programme Ex30Hard

---
+++ Ex30Hard/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 30: - HARD необязательная Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.. 
+// Далее надо посчитать количество нулей и единиц, если единиц больше чем нулей то вывести TRUE на экран, иначе вывести False.
+
+int[] array = new int[8];
+void FillArray(int[] collection)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,2);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array);
+PrintArray(array);
+
+int num1 = 0;
+int count = 8;
+for (int i = 0; i < count; i++)
+{
+    if (array [i] == 1) num1 = num1 + 1;
+}
+int num2 = count - num1;
+bool x = num1 > num2;
+Console.WriteLine(x);
