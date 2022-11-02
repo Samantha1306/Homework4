db85a69eb4599425393f3ba516e55e6f7d10a91c
Samantha <samantha@mail.com>
2022-11-02 20:00:13 +0300

    add programme Ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int SumNumbers(int x)
+{
+    int sum = 0;
+    while (x > 0)
+    {
+       int num = x % 10;
+        x = x / 10;
+        sum = sum + num;
+
+    }
+    
+    return sum;
+}
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма всех цифр в числе {n} равна {SumNumbers(n)}.");
